f54c2a09728796382f7bbb4f019a8987ed565e76
DESKTOP-JNKHTJE\ALEX <alenote01@gmail.com>
2023-01-04 11:21:35 -0800

    First Section

---
+++ src/components/FirstSection/FirstSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FirstSection.css';
+
+function FirstSection() {
+    return(
+        <div className='container-fluid' id='container'>
+            <h1>El futuro<br/>empieza hoy<br/>¿Ya lo estás<br/>creando?</h1>
+        </div>
+    );
+}
+
+export default FirstSection;  
