5552e79001b77693f7417c967f689d525b140f65
DESKTOP-JNKHTJE\ALEX <alenote01@gmail.com>
2023-01-04 12:27:34 -0800

    feat(Create): Border [COM-35]

---
+++ src/components/Border/Border.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Border.css';
+
+function Border() {
+    return(
+        <div className='container-fluid' id='border-container'>
+            <h1>El futuro es hoy • El futuro es hoy • El futuro es hoy • El futuro es hoy</h1>
+        </div>
+    );
+}
+
+export default Border; 
