4bed122abd46ca3b52f6815ce6967bee6ed7bf6c
Isaac <isaac.gz.ov@gmail.com>
2023-01-11 12:32:08 -0600

    chore(First): example

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -5,6 +5,7 @@ function First() {
     return(
         <div className='container-fluid main-container'>
             <h1>El futuro<br/>empieza hoy<br/>¿Ya lo estás<br/>creando?</h1>
+            <h2>test</h2>
         </div>
     );
 }
