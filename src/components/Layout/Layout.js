07778acb3bc86870f7f1dc28db3b3053676b2d84
FernandoGUGO <fernandogugon@gmail.com>
2023-01-18 09:28:52 -0600

    feat(MeetUp) Create meetup view [COM45]

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ROUTES } from "../../routes/routes";
 import Ecosistema from "../Pages/Ecosistema";
 import Startups from "../Pages/Startups";
-import Meetup from "../Pages/Meetup";
+import MeetUp from "../Pages/Comunidades/MeetUp/MeetUp";
 import Comunidades from "../Pages/Comunidades";
 import TheTeam from "../Pages/TheTeam";
 import Eventos from "../Pages/Eventos";
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
           <Route path={ROUTES.HOME} element={<Home />}></Route>
           <Route path={ROUTES.ECOSISTEMA} element={<Ecosistema />} />
           <Route path={ROUTES.STARTUPS} element={<Startups />} />
-          <Route path={ROUTES.MEETUP} element={<Meetup />} />
+          <Route path={ROUTES.MEETUP} element={<MeetUp />} />
           <Route path={ROUTES.COMUNIDADES} element={<Comunidades />} />
           <Route path={ROUTES.THETEAM} element={<TheTeam />} />
           <Route path={ROUTES.EVENTOS} element={<Eventos />} />
