0b1db462a2178fe91459dd7cbcbdf76877f08d42
Isaac <isaac.gz.ov@gmail.com>
2023-01-24 12:30:23 -0600

    chore(send-mail): updated request

--- src/api/send-mail.js
+++ src/api/send-mail.js
@@ -1,4 +1,4 @@
 import { axiosInstance } from "./axios.config";
 export const sendMail = async (mail) => {
-  return axiosInstance.get(`/user/recover-password/${mail}`);
+  return axiosInstance.post(`/user/recover-password`, { mail });
 };
